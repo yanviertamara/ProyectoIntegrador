e9c61cfff9368602d3a5834093304335da6c14c5
1102884264 <1102884264@SJLBF02050509.admcecar.local>
2020-02-26 17:49:11 -0500

    New template

---
+++ ProyectoIntegrador/src/main/java/edu/cecar/modelo/Persona.java
@@ -0,0 +1,26 @@
+/**
+  *Clase Persona.java
+  *
+  *@version:0.1
+  *
+  *Fecha de Creacion: 26/02/2020
+  *
+  *Fecha de Modificación:
+  *
+  *@author: 1102884264
+  *
+  *Copyright: CECAR
+  *
+*/
+
+package edu.cecar.modelo;
+
+public class Persona {
+
+}
+
+
+/**
+  *
+  *
+ */
