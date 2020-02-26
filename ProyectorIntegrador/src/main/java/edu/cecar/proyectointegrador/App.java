0f44ee072ba40b706d68e17ac99d98f12684e144
1102884264 <1102884264@SJLBF02050509.admcecar.local>
2020-02-26 17:03:52 -0500

    Se establecio la estructura del proyecto

---
+++ ProyectorIntegrador/src/main/java/edu/cecar/proyectointegrador/App.java
@@ -0,0 +1,13 @@
+package edu.cecar.proyectointegrador;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
